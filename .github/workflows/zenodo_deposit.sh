e7536c73296e5e891ea5c85f77c72bdf14975a37
wexeee <william.clarke@ndcn.ox.ac.uk>
2021-08-02 11:13:33 +0100

    Zenodo build.

---
+++ .github/workflows/zenodo_deposit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# From https://git.fmrib.ox.ac.uk/fsl/fsleyes/fsleyes/-/blob/master/.ci/zenodo_deposit.sh
+
+tmp=`dirname $0`
+pushd $tmp > /dev/null
+thisdir=`pwd`
+popd > /dev/null
+
+name=$1
+zenodo_url=$2
+zenodo_tkn=$3
+zenodo_depid=$4
+
+version=`python setup.py -V`
+upfile=`pwd`/dist/"$name"-"$version".tar.gz
+metafile=`pwd`/.ci/zenodo_meta.json.jinja2
+date=`date +"%Y-%m-%d"`
+
+pip install --retries 10 requests jinja2
+
+python "$thisdir"/zenodo.py \
+       "$zenodo_url" \
+       "$zenodo_tkn" \
+       "$zenodo_depid" \
+       "$upfile" \
+       "$metafile" \
+       "$version" \
+       "$date"
