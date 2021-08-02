2eea6a964e3f504db57580d0dc74a8c35664e5cc
wexeee <william.clarke@ndcn.ox.ac.uk>
2021-08-02 11:52:05 +0100

    specifiy font

--- .github/workflows/build_document.sh
+++ .github/workflows/build_document.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 version=`git tag`
 echo "${version}"
-pandoc document.MD -t html -o - | pandoc -f html --pdf-engine=xelatex -V geometry:margin=1in -o "my_built_document_${version}.pdf"
+pandoc document.MD -t html -o - | pandoc -f html --pdf-engine=xelatex -V geometry:margin=1in -V mainfont:Baskerville -o "my_built_document_${version}.pdf"
